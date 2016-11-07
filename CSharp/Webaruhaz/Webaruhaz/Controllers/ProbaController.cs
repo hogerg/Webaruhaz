9c623a2f0d97e7a30f22b063abfa74427948087f
Hodány Gergő <hogerg@gmx.com>
2016-11-07 03:38:04 +0100

    new C# project
    C# project created for website

---
+++ CSharp/Webaruhaz/Webaruhaz/Controllers/ProbaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webaruhaz.Controllers
+{
+    public class ProbaController : Controller
+    {
+        // GET: Proba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Aloldal()
+        {
+            return "aloldal probaszoveg";
+        }
+    }
+}
---
+++ CSharp/Webaruhaz/Webaruhaz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webaruhaz.Startup))]
+namespace Webaruhaz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
