69cd6f956c9f803d8226a2f32e79ccf22c586e45
Hodány Gergő <hogerg@gmx.com>
2016-11-08 00:15:43 +0100

    Design, CRUD
    Webshop felület elrendezése, termék CRUD

---
+++ CSharp/Webaruhaz/Webaruhaz/Controllers/TermekController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Webaruhaz.DAL;
+using Webaruhaz.Models;
+
+namespace Webaruhaz.Controllers
+{
+    public class TermekController : Controller
+    {
+        private TermekContext db = new TermekContext();
+
+        // GET: Termek
+        public ActionResult Index()
+        {
+            return View(db.Termekek.ToList());
+        }
+
+        // GET: Termek/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Termek termek = db.Termekek.Find(id);
+            if (termek == null)
+            {
+                return HttpNotFound();
+            }
+            return View(termek);
+        }
+
+        // GET: Termek/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Termek/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TermekID,Ar,Nev,Kategoria")] Termek termek)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Termekek.Add(termek);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(termek);
+        }
+
+        // GET: Termek/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Termek termek = db.Termekek.Find(id);
+            if (termek == null)
+            {
+                return HttpNotFound();
+            }
+            return View(termek);
+        }
+
+        // POST: Termek/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TermekID,Ar,Nev,Kategoria")] Termek termek)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(termek).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(termek);
+        }
+
+        // GET: Termek/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Termek termek = db.Termekek.Find(id);
+            if (termek == null)
+            {
+                return HttpNotFound();
+            }
+            return View(termek);
+        }
+
+        // POST: Termek/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Termek termek = db.Termekek.Find(id);
+            db.Termekek.Remove(termek);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CSharp/Webaruhaz/Webaruhaz/DAL/TermekInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Webaruhaz.Models;
+
+namespace Webaruhaz.DAL
+{
+    public class TermekInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<TermekContext>
+    {
+        protected override void Seed(TermekContext context)
+        {
+            var termekek = new List<Termek>
+            {
+            new Termek{TermekID=1111,Ar=12,Nev="Alma"},
+            new Termek{TermekID=2222, Ar=23,Nev="Korte"},
+            new Termek{TermekID=3333,Ar=34,Nev="Banan"},
+            new Termek{TermekID=4444,Ar=45,Nev="Citrom"},
+            new Termek{TermekID=5555,Ar=56,Nev="Eper"},
+            new Termek{TermekID=6666,Ar=67,Nev="Malna"},
+            new Termek{TermekID=7777,Ar=78,Nev="Meggy"},
+            new Termek{TermekID=8888,Ar=89,Nev="Cseresznye"}
+            };
+
+            termekek.ForEach(s => context.Termekek.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
